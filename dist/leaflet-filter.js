7487d843538afb8d5638a2c375e3cf3cd0ed38bf
Ryan Blace <reblace@gmail.com>
2017-04-10 22:54:05 -0400

    New version

--- dist/leaflet-filter.js
+++ dist/leaflet-filter.js
@@ -1,4 +1,4 @@
-/*! @asymmetrik/leaflet-filter-1.0.9 - Copyright Asymmetrik, Ltd. 2007-2017 - All Rights Reserved.*/
+/*! @asymmetrik/leaflet-filter-1.0.10 - Copyright Asymmetrik, Ltd. 2007-2017 - All Rights Reserved.*/
 
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
